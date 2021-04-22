b9a3c5da86d8f9321356fc56824e5e6f7ac8fc36
dex-coder <mhdkashifkhan028@gmail.com>
2021-04-22 16:34:51 +0500

    home

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from './rootReducer/rootReducer';
 
-const Auth = lazy(() => import('../features/auth/Auth'));
-const Home = lazy(() => import('../features/home/Home'));
+const Auth = lazy(() => import('./features/auth/Auth'));
+const Home = lazy(() => import('./features/home/Home'));
 
 const App: FC = () => {
   const isLoggedIn = useSelector(
