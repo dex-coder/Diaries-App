5e1ea46ae0c5c872e2ad388703854a2c7f1407d0
dex-coder <mhdkashifkhan028@gmail.com>
2021-04-25 16:10:10 +0500

    create diarytile

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,35 +1,34 @@
-import React, { FC, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { FC, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { User } from '../../interfaces/user.interface';
-// import * as Yup from "yup";
-// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import http from "../../services/api";
-import { saveToken, setAuthState } from "./authSlice";
-import { setUser } from "./userSlice";
-// import { AuthResponse } from "../../services/mirage/routes/user";
-import { useAppDispatch } from "../../store/store";
+import * as Yup from 'yup';
+import http from '../../services/api';
+import { saveToken, setAuthState } from './authSlice';
+import { setUser } from './userSlice';
+import { AuthResponse } from '../../services/mirage/routes/user';
+import { useAppDispatch } from '../../store/store';
 
-// const schema = Yup.object().shape({
-//   username: Yup.string()
-//     .required("What? No username?")
-//     .max(16, "Username cannot be longer than 16 characters"),
-//   password: Yup.string().required('Without a password, "None shall pass!"'),
-//   email: Yup.string().email("Please provide a valid email address (abc@xy.z)"),
-// });
+const schema = Yup.object().shape({
+  username: Yup.string()
+    .required('What? No username?')
+    .max(16, 'Username cannot be longer than 16 characters'),
+  password: Yup.string().required('Without a password, "None shall pass!"'),
+  email: Yup.string().email('Please provide a valid email address (abc@xy.z)'),
+});
 
 const Auth: FC = () => {
   const { handleSubmit, register, errors } = useForm<User>({
-    // validationSchema: schema,
+    validationSchema: schema,
   });
   const [isLogin, setIsLogin] = useState(true);
   const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
 
   const submitForm = (data: User) => {
-    const path = isLogin ? "/auth/login" : "/auth/signup";
+    const path = isLogin ? '/auth/login' : '/auth/signup';
     http
-      .post(path, data)
-      .then((res: any) => {
+      .post<User, AuthResponse>(path, data)
+      .then((res) => {
         if (res) {
           const { user, token } = res;
           dispatch(saveToken(token));
@@ -37,7 +36,7 @@ const Auth: FC = () => {
           dispatch(setAuthState(true));
         }
       })
-      .catch((error: any) => {
+      .catch((error) => {
         console.log(error);
       })
       .finally(() => {
@@ -48,17 +47,9 @@ const Auth: FC = () => {
   return (
     <div className="auth">
       <div className="card">
-        <h1 style={{ color: "#75869A", fontFamily: "serif" }}>
-          {isLogin ? "Login" : "Signup"}
-        </h1>
         <form onSubmit={handleSubmit(submitForm)}>
           <div className="inputWrapper">
-            <input
-              ref={register}
-              name="username"
-              placeholder="Username"
-              required
-            />
+            <input ref={register} name="username" placeholder="Username" />
             {errors && errors.username && (
               <p className="error">{errors.username.message}</p>
             )}
@@ -69,7 +60,6 @@ const Auth: FC = () => {
               name="password"
               type="password"
               placeholder="Password"
-              required
             />
             {errors && errors.password && (
               <p className="error">{errors.password.message}</p>
@@ -81,7 +71,6 @@ const Auth: FC = () => {
                 ref={register}
                 name="email"
                 placeholder="Email (optional)"
-                required
               />
               {errors && errors.email && (
                 <p className="error">{errors.email.message}</p>
@@ -90,14 +79,14 @@ const Auth: FC = () => {
           )}
           <div className="inputWrapper">
             <button type="submit" disabled={loading}>
-              {isLogin ? "Login" : "Create account"}
+              {isLogin ? 'Login' : 'Create account'}
             </button>
           </div>
           <p
             onClick={() => setIsLogin(!isLogin)}
-            style={{ cursor: "pointer", opacity: 0.7 }}
+            style={{ cursor: 'pointer', opacity: 0.7 }}
           >
-            {isLogin ? "No account? Create one" : "Already have an account?"}
+            {isLogin ? 'No account? Create one' : 'Already have an account?'}
           </p>
         </form>
       </div>
